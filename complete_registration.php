12c1b3e17dc5476e037b279fbf3b069909fd2aca
Sophiep1103 <92567819+Sophiep1103@users.noreply.github.com>
2023-11-09 13:45:09 +0200

    Add files via upload

---
+++ complete_registration.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="complete_registration.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Sofia+Sans&display=swap');
+        /* Additional CSS for positioning the icon */
+        .mail-icon {
+            margin-right: 5px; /* Adjust the margin as needed */
+            color: rgb(10, 10, 83);
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <h2><i class="fas fa-envelope mail-icon"></i>   Please check your email account for authentication mail</h2>
+</body>
+</html>
